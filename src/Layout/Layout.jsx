ab95b0b8e1274c0071f585276185769615b976c9
Calvin soares <calvinsoares17@gmail.com>
2023-12-08 11:49:15 -0300

    feat: local onde fica os componentes da tela

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header"
+import SectionOne from "../components/Hero/SectionOne"
+
+export default function Layout() {
+  return (
+    <div>
+      <Header />
+      <SectionOne />
+    </div>
+        
+  )
+
+}
